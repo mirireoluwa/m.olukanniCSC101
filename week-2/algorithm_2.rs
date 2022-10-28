316d7f4c5e905fea6d73b38b99e6a09b6c19c9de
mirireoluwa olukanni <115805891+mothesaintt@users.noreply.github.com>
2022-10-28 16:32:26 +0100

    Update algorithm_2.rs

--- week-2/algorithm_2.rs
+++ week-2/algorithm_2.rs
@@ -1,9 +1,9 @@
 fn main() {
-	let toshiba:f64 = 450_000.00;
-	let mac:f64 = 1_500_000.00;
-	let hp:f64 = 750_000.00;
-	let dell:f64 = 2_850_000.00;
-	let acer:f64 = 250_000.00;
+	let toshiba:f64 = 450_000.00 * 2;
+	let mac:f64 = 1_500_000.00 * 1;
+	let hp:f64 = 750_000.00 * 3;
+	let dell:f64 = 2_850_000.00 * 3;
+	let acer:f64 = 250_000.00 * 1;
 	
 	//sum
 	let sum = toshiba + mac + hp + dell + acer;
