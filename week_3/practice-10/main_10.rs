4b7e3effd8ee1000ea967edcedbccb20a1936eae
mirireoluwa olukanni <115805891+mothesaintt@users.noreply.github.com>
2022-11-04 19:11:48 +0100

    My Third Commit

---
+++ week_3/practice-10/main_10.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // addition
+    let sum = 5_550.0 + 7_310.0;
+    println!("The sum of 5_550.0 and 7_310 = {}", sum);
+
+    // subtraction
+    let difference:f32 = 95.5 - 4.3;
+    println!("The difference of 95.5 and 4.3 = {}", difference);
+
+    // multipication
+    let product:f32 = 4.0 * 30.0;
+    println!("The product of 4 and 30 = {}", product);
+
+    // division
+    let quotient = 56.7  / 32.2;
+    println!("The quotient of 56.7 and 32.2 = {}", quotient);
+
+    // remainder
+    let remainder = 43.0/5.0;
+    println!("The remainder of 43.0 and 5.0 = {}", remainder);
+
+}
