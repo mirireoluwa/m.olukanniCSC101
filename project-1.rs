d812aeec5d82e7262176f4ad9716a9cb9aa4db0c
mirireoluwa olukanni <115805891+mothesaintt@users.noreply.github.com>
2022-12-14 23:52:15 +0100

    1st project in week 9

--- project-1.rs
+++ project-1.rs
@@ -1,64 +1,46 @@
-// Rust program to find the roots of a quadratic equation
+use std::io::Write;
 
-use std::io;
+fn main() {
+    //Lager type
+    let lager = vec!["Lager", "33 Export", "Desperados", "Goldberg", "Gulder", "Heineken", "Star"];
 
-fn main() 
-{
-    let _a:f32 = 0.0;
-    let _b:f32 = 0.0;
-    let _c:f32 = 0.0;
+    let stout = vec!["Stout", "Legend", "Turbo King", "Williams"];
 
-    let _root1:f32 = 0.0;
-    let _root2:f32 = 0.0;
+    let n_alcoholic = vec!["Non-Alcoholic", "Maltina", "Amstel Malta", "Malta Gold", "Fayrouz"];
 
-    let _realroots:f32 = 0.0;
-    let _imaginaryroots:f32 = 0.0;
-    let _discriminant:f32 = 0.0;
+            let mut lager_str = String::new();
+            for i in 0..lager.len()
+            {
+            lager_str +=  &lager[i];
+            lager_str += "\n";
+            }
+            lager_str +=  "\n\n\n";
 
-    let mut input1 = String::new();
-    let mut input2 = String::new();
-    let mut input3 = String::new();
-
-    println!("Enter value of a: ");
-    io::stdin().read_line(&mut input1).expect("Not a valid string");
-    let a:f32 = input1.trim().parse().expect("Not a valid number");
-
-    println!("Enter value of b: ");
-    io::stdin().read_line(&mut input2).expect("Not a valid string");
-    let b:f32 = input2.trim().parse().expect("Not a valid number");
-
-    println!("Enter value of c: ");
-    io::stdin().read_line(&mut input3).expect("Not a valid string");
-    let c:f32 = input3.trim().parse().expect("Not a valid number");
-
-    let discriminant:f32 = (b * b) - (4.0 * a * c);
-
-    if discriminant > 0.0
+    let mut stout_str = String::new();
+    for i in 0..stout.len()
     {
-        println!("Real and distinct, they are:");
-        let root1 = (-b + discriminant.sqrt()) / (2.0 * a);
-        let root2 = (-b - discriminant.sqrt()) / (2.0 * a);
-        println!("Root 1 = {}", root1);
-        println!("Root 2 = {}", root2);
-    }
-    else if discriminant < 0.0
-    {
-        println!("Imaginary roots, they are:");
-        let root1 = (-b + discriminant.sqrt()) / (2.0 * a);
-        let root2 = (-b - discriminant.sqrt()) / (2.0 * a);
-        println!("Root 1 = {}", root1);
-        println!("Root 2 = {}", root2);
-    }
-    else if discriminant == 0.0
-    {
-        println!("One real root :");
-        let root1 = (-b + discriminant.sqrt()) / (2.0 * a);
-        let root2 = (-b - discriminant.sqrt()) / (2.0 * a);
-        println!("Root 1 = {}", root1);
-        println!("Root 2 = {}", root2);
+        stout_str += &stout[i];
+        stout_str += "\n";
     }
 
+    stout_str +=  "\n\n\n";
 
+
+    let mut n_alcoholic_str = String::new();
+    for i in 0..n_alcoholic.len()
+    {
+        n_alcoholic_str += &n_alcoholic[i];
+        n_alcoholic_str += "\n";
+    }
+
+    n_alcoholic_str +=  "\n\n\n";
+
+   let mut file = std::fs::File::create("project-1.txt").expect("create failed");
+    file.write_all("Welcome to Nigerian Brewery Limited! (Inc.1946)\n Here are some high quality drinks we have:\n"
+        .as_bytes()).expect("write failed");
+    file.write_all(lager_str.as_bytes()).expect("write failed");
+    file.write_all(stout_str.as_bytes()).expect("write failed");
+    file.write_all(n_alcoholic_str.as_bytes()).expect("write failed");
+    println!("\nData Written to File." );
+    
 }
-
-
