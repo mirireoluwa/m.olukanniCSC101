38c51427348f5f8973fb4fa8c676d82d3bcc80dd
mirireoluwa olukanni <115805891+mothesaintt@users.noreply.github.com>
2023-03-08 23:07:35 +0100

    python basics
    python basics

---
+++ saintted.py
@@ -0,0 +1,6 @@
+print("Hello, my name is Saint the Angel!")
+name = input("What's your name? ")
+if name == "Mirireoluwa":
+    print("That's an amazing name", name)
+else:
+    print("Hmm, that's an odd name")
